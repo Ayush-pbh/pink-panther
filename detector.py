67bb4a2235457bdb8594ec9307de7898cac01527
Ayush Tripathi <yush.pbh@gmail.com>
2023-11-08 23:37:07 +0530

    dd

---
+++ detector.py
@@ -0,0 +1,166 @@
+# detector.py
+
+from pathlib import Path
+import pickle
+import face_recognition
+from collections import Counter
+from PIL import Image, ImageDraw
+import argparse
+import cv2
+
+DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+
+Path("training").mkdir(exist_ok=True)
+Path("output").mkdir(exist_ok=True)
+Path("validation").mkdir(exist_ok=True)
+
+
+parser = argparse.ArgumentParser(description="Recognize faces in an image")
+parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument(
+    "--validate", action="store_true", help="Validate trained model"
+)
+parser.add_argument(
+    "--test", action="store_true", help="Test the model with an unknown image"
+)
+parser.add_argument(
+    "-m",
+    action="store",
+    default="hog",
+    choices=["hog", "cnn"],
+    help="Which model to use for training: hog (CPU), cnn (GPU)",
+)
+parser.add_argument(
+    "-f", action="store", help="Path to an image with an unknown face"
+)
+parser.add_argument(
+    "--stream", action="store_true", help="Live Stream Detection"
+)
+args = parser.parse_args()
+
+def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH) -> None:
+    names = []
+    encodings = []
+
+    for filepath in Path("training").glob("*/*"):
+        name = filepath.parent.name
+        image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+    name_encodings = {"names": names, "encodings": encodings}
+    with encodings_location.open(mode="wb") as f:
+        pickle.dump(name_encodings, f)
+
+def _recognize_face(unknown_encoding, loaded_encodings):
+    boolean_matches = face_recognition.compare_faces(
+        loaded_encodings["encodings"], unknown_encoding
+    )
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
+BOUNDING_BOX_COLOR = "blue"
+TEXT_COLOR = "white"
+
+# ...
+
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox(
+        (left, bottom), name
+    )
+    draw.rectangle(
+        ((text_left, text_top), (text_right, text_bottom)),
+        fill="blue",
+        outline="blue",
+    )
+    draw.text(
+        (text_left, text_top),
+        name,
+        fill="white",
+    )
+def recognize_faces(image_location: str,model: str = "hog",encodings_location: Path = DEFAULT_ENCODINGS_PATH,) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
+    input_face_locations = face_recognition.face_locations(
+        input_image, model=model
+    )
+    input_face_encodings = face_recognition.face_encodings(
+        input_image, input_face_locations
+    )
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        # Removed print(name, bounding_box)
+        _display_face(draw, bounding_box, name)
+
+    del draw
+    pillow_image.show()
+
+def validate(model: str = "hog"):
+    for filepath in Path("validation").rglob("*"):
+        if filepath.is_file():
+            recognize_faces(
+                image_location=str(filepath.absolute()), model=model
+            )
+
+
+def recognize_faces_live(model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH):
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    stream_url = 0
+    video_capture = cv2.VideoCapture(stream_url)  # Use the default camera (change to a different source if needed)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        face_locations = face_recognition.face_locations(frame, model=model)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+        for bounding_box, unknown_encoding in zip(face_locations, face_encodings):
+            name = _recognize_face(unknown_encoding, loaded_encodings)
+            if not name:
+                name = "Unknown"
+
+            top, right, bottom, left = bounding_box
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        cv2.imshow("Video", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)
+    if args.stream:
+        recognize_faces_live(model=args.m)
